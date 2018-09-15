ed6ebce5d161b957c655cfbb18c54f11f78c74ae
Vui Nguyen <vui_engineer@yahoo.com>
2018-09-15 09:59:00 -0600

    before refactoring

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,31 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
-  var recordedAudioURL: URL!
+  var recordedAudioURL:URL!
+  var audioFile:AVAudioFile!
+  var audioEngine:AVAudioEngine!
+  var audioPlayerNode: AVAudioPlayerNode!
+  var stopTimer: Timer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  enum ButtonType: Int {
+    case slow = 0, fast, chipmunk, vader, echo, reverb
+  }
 
-        // Do any additional setup after loading the view.
+  // MARK: Outlets
+
+  @IBOutlet weak var snailButton: UIButton!
+  @IBOutlet weak var chipmunkButton: UIButton!
+  @IBOutlet weak var rabbitButton: UIButton!
+  @IBOutlet weak var vaderButton: UIButton!
+  @IBOutlet weak var echoButton: UIButton!
+  @IBOutlet weak var reverbButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
+
+  // MARK: Actions
+
+  @IBAction func playSoundForButton(_ sender: UIButton) {
+    switch(ButtonType(rawValue: sender.tag)!) {
+    case .slow:
+      playSound(rate: 0.5)
+    case .fast:
+      playSound(rate: 1.5)
+    case .chipmunk:
+      playSound(pitch: 1000)
+    case .vader:
+      playSound(pitch: -1000)
+    case .echo:
+      playSound(echo: true)
+    case .reverb:
+      playSound(reverb: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    configureUI(.playing)
+  }
 
-    /*
-    // MARK: - Navigation
+  @IBAction func stopButtonPressed(_ sender: AnyObject) {
+    stopAudio()
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupAudio()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    configureUI(.notPlaying)
+  }
 
 }
